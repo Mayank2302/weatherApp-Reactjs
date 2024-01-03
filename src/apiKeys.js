2b29501c66ea769e0a877ea6588a8bd194d2719a
Mayank <mayankmalviya332@gmail.com>
2024-01-03 23:33:33 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a7f44f1a26c1925b6b3e802d78b09a09",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
